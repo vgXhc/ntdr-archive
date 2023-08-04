d58daaa2cbb4a6797ac2495f812b7ce7a81590d9
Harald Kliems <kliems@gmail.com>
2023-08-04 15:38:22 -0500

    initial code ready

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ntdr-archive
 Archiving National Transit Database Data
+
+This repository hosts code for archiving data from the National Transit Database, using the R packages `ntdr` and `pins`. A Github action runs the code daily and writes updated data to a `pins` board hosted on Amazon S3. 
---
+++ update_ntdr.R
@@ -0,0 +1,28 @@
+library(pins)
+library(ntdr)
+
+
+
+ntdr_board <- board_s3(bucket = "ntdr-pins",
+         versioned = TRUE,
+         access_key = Sys.getenv("PINS_S3_KEY"), 
+         secret_access_key = Sys.getenv("PINS_S3_PASSWORD"),
+         region = "us-west-2")
+
+upt_adjusted <- get_ntd(data_type = "adjusted", ntd_variable = "UPT", cache = TRUE)
+upt_raw <- get_ntd(data_type = "raw", ntd_variable = "UPT", cache = TRUE)
+vrm_adjusted <- get_ntd(data_type = "adjusted", ntd_variable = "VRM", cache = TRUE)
+vrm_raw <- get_ntd(data_type = "raw", ntd_variable = "VRM", cache = TRUE)
+vrh_adjusted <- get_ntd(data_type = "adjusted", ntd_variable = "VRH", cache = TRUE)
+vrh_raw <- get_ntd(data_type = "raw", ntd_variable = "VRH", cache = TRUE)
+voms_adjusted <- get_ntd(data_type = "adjusted", ntd_variable = "VOMS", cache = TRUE)
+voms_raw <- get_ntd(data_type = "raw", ntd_variable = "VOMS", cache = TRUE)
+
+pin_write(board = ntdr_board, x = upt_adjusted)
+pin_write(board = ntdr_board, x = upt_raw)
+pin_write(board = ntdr_board, x = vrm_adjusted)
+pin_write(board = ntdr_board, x = vrm_raw)
+pin_write(board = ntdr_board, x = vrh_adjusted)
+pin_write(board = ntdr_board, x = vrh_raw)
+pin_write(board = ntdr_board, x = voms_adjusted)
+pin_write(board = ntdr_board, x = voms_raw)
